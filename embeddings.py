33f5c54809c5c8c129e3367d8c7486c42b819911
Sammi <sammizhu@college.harvard.edu>
2025-03-12 03:16:02 -0400

    added API key

--- embeddings.py
+++ embeddings.py
@@ -1,13 +1,13 @@
 #https://www.youtube.com/watch?v=RiaundHy1Xc
 from together import Together
 
-client = Together()
+client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
 
 response = client.files.upload(file="")
 fileId = response.model_dump()["id"]
 
 resp = client.fine_tuning.create(
-    model="",
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
     training_file=fileId,
     n_epoches=3,
     learning_rate=1e-5,
